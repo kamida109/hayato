9722529031a223ae34db8a778ffcec8c7f76be80
kamida <21kamida_ryuusei@axiz.co.jp>
2021-06-16 09:24:07 +0900

    getter,setter

--- src/main/java/jp/co/axiz/web/entity/Hayato.java
+++ src/main/java/jp/co/axiz/web/entity/Hayato.java
@@ -10,9 +10,17 @@ public class Hayato {
 	public int getMoney() {
 		return money;
 	}
+
 	public void setMoney(int money) {
 		this.money = money;
 	}
 
+	public int getHayato() {
+		return hayato;
+	}
+
+	public void setHayato(int hayato) {
+		this.hayato = hayato;
+	}
 
 }

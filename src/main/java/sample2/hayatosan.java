c44af191a92babd77a7d80a381bb32bbc211eda7
User <User@WINDOWS-61M4L6L>
2021-06-17 10:44:08 +0900

    逸人さんのアクセサーを作成

---
+++ src/main/java/sample2/hayatosan.java
@@ -0,0 +1,15 @@
+package sample2;
+
+public class hayatosan {
+
+	private int hayatosan;
+
+	public int getHayatosan() {
+		return hayatosan;
+	}
+
+	public void setHayatosan(int hayatosan) {
+		this.hayatosan = hayatosan;
+	}
+
+}
